44812bbd440bd131d0a728d923be83eda9e58b0e
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-24 09:27:45 -0600

    159. Mostrar registros en el historial

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DataLocalService } from '../../services/data-local.service';
+import { Subscription } from 'rxjs';
+import { Registro } from '../../models/registro.model';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit, OnDestroy {
 
-  constructor() {}
+  public registros: Registro[] = [];
+
+  private registrosSub: Subscription;
+
+  constructor(
+    private dataLocalService: DataLocalService
+  ) {}
+
+  sendMail(): void {
+    console.log('sendMail');
+  }
+
+  ngOnInit() {
+    this.registros = this.dataLocalService.getRegistros();
+    this.registrosSub = this.dataLocalService.registrosChanged.subscribe(() => this.registros = this.dataLocalService.getRegistros());
+  }
+
+  openRegistro(registro: Registro): void {
+    console.log(registro);
+  }
+
+  ngOnDestroy() {
+    this.registrosSub.unsubscribe();
+  }
 
 }
--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Registro } from '../models/registro.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,20 @@ export class DataLocalService {
 
   private registros: Registro[] = [];
 
+  public registrosChanged = new Subject<void>();
+
   constructor() { }
+
+  getRegistros(): Registro[] {
+    return this.registros.slice();
+  }
 
   save(format: string, text: string) {
 
     const registro = new Registro(format, text);
+
     this.registros.unshift(registro);
+    this.registrosChanged.next();
 
     console.log(this.registros);
 
